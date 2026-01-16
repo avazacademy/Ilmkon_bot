c30291ccd0ed549185a4d1d82e5ca0a69dc3afdf
XusanDev07 <133357417+XusanDev07@users.noreply.github.com>
2026-01-16 20:29:09 +0500

    Refactor add_user function to always create a new user by removing duplicate check

--- database/requests.py
+++ database/requests.py
@@ -3,24 +3,22 @@ from sqlalchemy import select
 
 async def add_user(data: dict, tg_id: int):
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.telegram_id == tg_id))
-        
-        if not user:
-            session.add(User(
-                telegram_id=tg_id,
-                familiya=data['familiya'],
-                ism=data['ism'],
-                ota_ismi=data['ota_ismi'],
-                jins=data['jins'],
-                tugilgan_kun=data['tugilgan_kun'],
-                telefon=data['telefon'],
-                viloyat=data['viloyat'],
-                tuman=data['tuman'],
-                maktab=data['maktab'],
-                sinf=data['sinf'],
-                fan=data['fan'],
-                til=data['til'],
-                rasm_id=data['photo'],
-                check_id=data['check'] # YANGI
-            ))
-            await session.commit()
+        # Har safar yangi user yaratamiz (duplicate tekshiruvini olib tashladik)
+        session.add(User(
+            telegram_id=tg_id,
+            familiya=data['familiya'],
+            ism=data['ism'],
+            ota_ismi=data['ota_ismi'],
+            jins=data['jins'],
+            tugilgan_kun=data['tugilgan_kun'],
+            telefon=data['telefon'],
+            viloyat=data['viloyat'],
+            tuman=data['tuman'],
+            maktab=data['maktab'],
+            sinf=data['sinf'],
+            fan=data['fan'],
+            til=data['til'],
+            rasm_id=data['photo'],
+            check_id=data['check']
+        ))
+        await session.commit()
